373b7d1f4f8f7708280356c7078f6b3e024ada31
Calum Darroch <calum_darroch@hotmail.com>
2019-04-18 16:53:30 +0100

    Added sign_in_and_play test helper

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature "hit points" do
   scenario "hit points are displayed next to player 2" do
-    visit('/')
-    fill_in :player_1_name, with: 'Conan'
-    fill_in :player_2_name, with: 'Dutch'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Dutch: 100HP'
   end
 end
